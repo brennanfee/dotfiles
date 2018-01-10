148ce8a6c42818a641803bd3f63f98bf5def04e2
Brennan Fee <bfee@uship.com>
2018-01-10 10:56:26 -0600

    Updating Tmux settings.
    Now using the Tmux Package Manager with some plugins.

--- bash/shared/path.bash
+++ bash/shared/path.bash
@@ -2,6 +2,7 @@
 
 # Set PATH so it includes user's home bin folders (if they exist)
 # NOTE: The order of these is important, the last one will be searched first
+path_prepend "${HOME}/.local/bin"
 path_prepend "$DOTFILES/bin"
 path_prepend "$DOTFILES_PRIVATE/bin"
 path_prepend "${HOME}/bin"
--- bash/windows/aliases/apps.bash
+++ bash/windows/aliases/apps.bash
@@ -2,3 +2,12 @@
 # Aliases for linux apps, redirecting to windows versions
 alias gvim="/mnt/c/Program\ Files\ \(x86\)/vim/vim80/gvim.exe"
 
+alias docker="docker.exe"
+alias docker-compose="docker-compose.exe"
+alias docker-machine="docker-machine.exe"
+
+alias start="cmd.exe /c start"
+alias open="cmd.exe /c start"
+alias xdg-open="cmd.exe /c start"
+alias o="cmd.exe /c start"
+
