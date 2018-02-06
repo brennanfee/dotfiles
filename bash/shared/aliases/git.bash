4e762b5dfa151a4ff8fcf9a220119751139370da
Brennan Fee <bfee@uship.com>
2018-02-06 13:42:18 -0600

    Support for github hub command line.
    Switching to use the Hub command which provides some github specific
    helpers around git.  Also updating both shell and git aliases.

--- bash/shared/aliases/git.bash
+++ bash/shared/aliases/git.bash
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-alias g="git"
-alias get="git"
+local git_cmd="git"
+if command_exists hub; then
+    git_cmd="hub"
+    eval "$(hub alias -s)"
+fi
 
-alias gs="git status"
-alias gss="git status -sb"
+alias g="$git_cmd"
+alias get="$git_cmd"
 
-alias gexp='git archive --format zip --output'
+alias gs="$git_cmd status"
+alias gss="$git_cmd status -sb"
 
-alias gll='git log --graph --pretty=oneline --abbrev-commit'
+alias gexp='$git_cmd archive --format zip --output'
+
+alias gll='$git_cmd log --graph --pretty=oneline --abbrev-commit'
