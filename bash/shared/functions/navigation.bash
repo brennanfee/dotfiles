636940a50fb3246b731a6e372f46d56263c3bc75
Brennan Fee <github@todaytechsoft.biz>
2018-03-30 23:26:17 -0500

    Adding cdt to navigation, fixing cdpp.
    Adding some fallbacks for my navigation commands.

--- bash/shared/functions/navigation.bash
+++ bash/shared/functions/navigation.bash
@@ -5,28 +5,65 @@ export MY_PROFILE=$HOME
 # This is only here for Windows and WSL.  For all non-Windows machines HOME is home, but for
 # the WSL shell home is the unix home and OS_HOME gets set to the machines user
 # profile (/mnt/c/Users/<name>).  This allows cd on its own to take you to the unix home but cdh
-# to take you to the "windows" home.
+# to take you to the "windows" home.  On a linux box cdh is equivelant to cd.
 export OS_HOME=$HOME
 
 # Some quick navigations
 function cdp {
-    cd "$MY_PROFILE/projects"
+    if [[ -d "$MY_PROFILE/projects" ]]; then
+        cd "$MY_PROFILE/projects"
+    else
+        cd "$MY_PROFILE"
+    fi
 }
 
 function cdpp {
-    cd "$MY_PROFILE/projects/personal"
+    if [[ -d "$MY_PROFILE/projects/personal" ]]; then
+        cd "$MY_PROFILE/projects/personal"
+    else
+        if [[ -d "$MY_PROFILE/projects" ]]; then
+            cd "$MY_PROFILE/projects"
+        else
+            cd "$MY_PROFILE"
+        fi
+    fi
 }
 
 function cdd {
-    cd "$MY_PROFILE/downloads"
+    if [[ -d "$MY_PROFILE/downloads" ]]; then
+        cd "$MY_PROFILE/downloads"
+    else
+        cd "$MY_PROFILE"
+    fi
 }
 
 function cdi {
     cd "$MY_PROFILE/downloads/installs"
+    if [[ -d "$MY_PROFILE/downloads/installs" ]]; then
+        cd "$MY_PROFILE/downloads/installs"
+    else
+        if [[ -d "$MY_PROFILE/downloads" ]]; then
+            cd "$MY_PROFILE/downloads"
+        else
+            cd "$MY_PROFILE"
+        fi
+    fi
+}
+
+function cdt {
+    if [[ -d "$HOME/.dotfiles" ]]; then
+        cd "$HOME/.dotfiles"
+    else
+        cd "$HOME"
+    fi
 }
 
 function cdh {
-    cd "$OS_HOME"
+    if [[ -d "$OS_HOME" ]]; then
+        cd "$OS_HOME"
+    else
+        cd "$HOME"
+    fi
 }
 
 alias cdr="cd-to-git-root-path"
