de81155a197edae479c1e40570d2375aeae5eb65
Brennan Fee <github@todaytechsoft.biz>
2018-04-01 22:12:20 -0500

    Changing detection for distros and WSL (windows).
    I'm splitting out the distinction of OS and whether I'm running in WSL.
    WSL will now be treated as a special case with scripts that run last
    over any os and distro-specific scripts.  Also fixed the distro
    detection, which broke when I was trying to incorporate WSL into the
    detection process.  Now using the fairly standard /etc/os-release to
    determine the Linux distribution.

--- bash/base-functions.bash
+++ bash/base-functions.bash
@@ -1,5 +1,51 @@
 #!/usr/bin/env bash
 
+# Function that sets two environment variables to indicate the
+# type of machine we are on.
+function SetOsEnvironmentVariables {
+    local uname=$(uname -s | tr '[:upper:]' '[:lower:]')
+    if [[ $uname == "darwin" ]]; then
+        export OS_PRIMARY="macos"
+        export OS_SECONDARY="macos"
+    elif [[ $uname == "linux" ]]; then
+        local ver=$(cat /etc/os-release | grep -i '^ID=' | sed -e 's/^ID=//' | tr '[:upper:]' '[:lower:]')
+        export OS_PRIMARY="linux"
+        export OS_SECONDARY=$(cat /etc/os-release | grep -i '^ID=' | sed -e 's/^ID=//' | tr '[:upper:]' '[:lower:]')
+        if [[ "${OS_SECONDARY}x" == "x" ]]; then
+            export OS_SECONDARY="unknown"
+        fi
+    elif [[ $uname == "freebsd" ]]; then
+        export OS_PRIMARY="bsd"
+        export OS_SECONDARY="freebsd"
+    elif [[ $uname == "openbsd" ]]; then
+        export OS_PRIMARY="bsd"
+        export OS_SECONDARY="openbsd"
+    else
+        export OS_PRIMARY="unknown"
+        export OS_SECONDARY="unknown"
+    fi
+
+    # Also check to see if we are runnin on Windows in WSL
+    local kernel=$(uname -r | tr '[:upper:]' '[:lower:]')
+    if [[ $kernel == *"microsoft"* ]]; then
+        export IS_WSL="true"
+    else
+        export IS_WSL="false"
+    fi
+}
+
+function is_mac {
+    return [[ $OS_PRIMARY == "macos" ]]
+}
+
+function is_linux {
+    return [[ $OS_PRIMARY == "linux" ]]
+}
+
+function is_windows {
+    return [[ $IS_WSL == "true" ]]
+}
+
 function SetVirtualizationEnvironmentVariables() {
     if [[ $(systemd-detect-virt --vm) == "none" ]]; then
         export IS_VM="false"
@@ -26,61 +72,6 @@ function SetVirtualizationEnvironmentVariables() {
     else
         export IS_VAGRANT="true"
     fi
-}
-
-# Function that sets two environment variables to indicate the
-# type of machine we are on.
-function SetOsEnvironmentVariables {
-    local uname=$(uname -s | tr '[:upper:]' '[:lower:]')
-    if [[ $uname == "darwin" ]]; then
-        export OS_PRIMARY="macos"
-        export OS_SECONDARY="macos"
-    elif [[ $uname == "linux" ]]; then
-        local ver=$(cat /proc/version | tr '[:upper:]' '[:lower:]')
-        export OS_PRIMARY="linux"
-        if [[ $ver == *"-arch"* ]]; then
-            export OS_SECONDARY="arch"
-        elif [[ $ver == *".centos.org"* ]]; then
-            export OS_SECONDARY="centos"
-        elif [[ $ver == *".fedoraproject.org"* ]]; then
-            export OS_SECONDARY="fedora"
-        elif [[ $ver == *"-kalil"* ]]; then
-            export OS_SECONDARY="kali"
-        elif [[ $ver == *"(suse "* ]]; then
-            export OS_SECONDARY="suse"
-        elif [[ $ver == *"(ubuntu "* ]]; then
-            export OS_SECONDARY="ubuntu"
-        elif [[ $ver == *"(debian "* ]]; then
-            export OS_SECONDARY="debian"
-        elif [[ $ver == *"(microsoft"* ]]; then
-            export OS_SECONDARY="windows"
-        else
-            export OS_SECONDARY="unknown"
-        fi
-    elif [[ $uname == "freebsd" ]]; then
-        export OS_PRIMARY="bsd"
-        export OS_SECONDARY="freebsd"
-    elif [[ $uname == "openbsd" ]]; then
-        export OS_PRIMARY="bsd"
-        export OS_SECONDARY="openbsd"
-    else
-        export OS_PRIMARY="unknown"
-        export OS_SECONDARY="unknown"
-    fi
-
-    SetVirtualizationEnvironmentVariables
-}
-
-function is_mac {
-    return [[ $OS_PRIMARY == "macos" ]]
-}
-
-function is_linux {
-    return [[ $OS_PRIMARY == "linux" ]]
-}
-
-function is_windows {
-    return [[ $OS_PRIMARY == "windows" ]]
 }
 
 # Function to easily add a path (this version adds to the end)
