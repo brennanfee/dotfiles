eb04f48a9fe89c7d5af4e60040730e655a2a211c
Brennan Fee <github@todaytechsoft.biz>
2018-04-22 02:39:07 -0500

    Linted identity.bash, fixed an optimization

--- bash/shared/exports/identity.bash
+++ bash/shared/exports/identity.bash
@@ -7,7 +7,7 @@ export WORK_EMAIL="bfee@uship.com"
 
 export EMAIL="$HOME_EMAIL"
 
-rc_tags=$(cat "$HOME/.rcrc" | grep -i '^TAGS=' | sed -e 's/^TAGS=//' | tr '[:upper:]' '[:lower:]')
+rc_tags=$(grep -i '^TAGS=' "$HOME/.rcrc" | sed -e 's/^TAGS=//' | tr '[:upper:]' '[:lower:]')
 if [[ $rc_tags == *"work"* ]]; then
     export EMAIL="$WORK_EMAIL"
 fi
