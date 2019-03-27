fc13c32223846be65bec2d417b38eb43fe6239f8
Brennan Fee <febrenna@amazon.com>
2019-03-26 21:51:29 -0500

    Updating dev tools, adding default packages

--- bash/shared/other/path.bash
+++ bash/shared/other/path.bash
@@ -3,12 +3,6 @@
 # Set PATH so it includes user's home bin folders (if they exist)
 # NOTE: The order of these is important, the last one will be searched first
 
-# Python
-path_prepend "${HOME}/.local/bin"
-if command_exists poetry; then
-  path_prepend "${HOME}/.poetry/bin"
-fi
-
 # Dotfiles
 path_prepend "$DOTFILES/bin"
 path_prepend "$DOTFILES_PRIVATE/bin"
@@ -19,10 +13,21 @@ path_prepend "${HOME}/bin"
 # WSL (Windows)
 [[ -d "$WIN_HOME/winfiles/bin" ]] && path_append "${WIN_HOME}/winfiles/bin"
 
+# Python
+path_append "${HOME}/.local/bin"
+if command_exists poetry; then
+  path_append "${HOME}/.poetry/bin"
+fi
+
 # Node
 if command_exists yarn; then
   path_append "$(yarn global bin)"
 fi
 if command_exists npm; then
   path_append "$(npm config --global get prefix)/bin"
+fi
+
+# Ruby & Gems
+if command_exists ruby && command_exists gem; then
+  path_append "$(ruby -r rubygems -e 'puts Gem.user_dir')/bin"
 fi
