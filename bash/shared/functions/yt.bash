c34ab446b37a29065d29fba1d9a823b5e07de047
Brennan Fee <brennan@github.mailbots.org>
2019-05-04 13:21:53 -0500

    Changed output locations for youtube-dl
    Now using xdg-user-dir to find paths.  Added pt to provide a second
    location for downloads separate from yt.

--- bash/shared/functions/yt.bash
+++ bash/shared/functions/yt.bash
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-function yt() {
+function yt-helper() {
+  local url
   if [[ $IS_WSL == "1" ]]; then
     url=$(paste.exe)
     echo "Downloading $url"
-    youtube-dl.exe "$url"
+    youtube-dl.exe -o "$1" "$url"
   else
     url=$(xsel -o --clipboard)
     echo "Downloading $url"
     if command_exists aria2c; then
-      youtube-dl --external-downloader=aria2c "$url"
+      youtube-dl --external-downloader=aria2c -o "$1" "$url"
     else
-      youtube-dl "$url"
+      youtube-dl -o "$1" "$url"
     fi
   fi
 }
+
+function yt() {
+  local output
+  output="$(xdg-user-dir VIDEOS)/%(title)s.%(ext)s"
+  yt-helper "$output"
+}
+
+function pt() {
+  local output
+  output="$(xdg-user-dir PICTURES)/!other/%(title)s.%(ext)s"
+  yt-helper "$output"
+}
+
