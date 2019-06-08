cf52c37978d03844d515e0aa95a8cfe157e4f387
Brennan Fee <brennan@github.mailbots.org>
2019-06-08 01:35:52 -0500

    Added ytm, audio version of yt
    Allows me to extract the audio track of a youtube video directly.

--- bash/shared/functions/yt.bash
+++ bash/shared/functions/yt.bash
@@ -32,3 +32,28 @@ function pt() {
   output="$(xdg-user-dir PICTURES)/!other/%(title)s.%(ext)s"
   yt-helper "$output"
 }
+
+# To extract an MP3 from a video, requires ffmpeg to be installed
+function ytm() {
+  local output
+  output="$(xdg-user-dir VIDEOS)/%(title)s.%(ext)s"
+
+  local url
+  if [[ $IS_WSL == "1" ]]; then
+    url=$(powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Get-Clipboard)
+    echo "Downloading $url"
+    if command_exists aria2c.exe; then
+      youtube-dl.exe --extract-audio --audio-format mp3 --external-downloader=aria2c.exe -o "$output" "$url"
+    else
+      youtube-dl.exe --extract-audio --audio-format mp3 -o "$output" "$url"
+    fi
+  else
+    url=$(xsel -o --clipboard)
+    echo "Downloading $url"
+    if command_exists aria2c; then
+      youtube-dl --extract-audio --audio-format mp3 --external-downloader=aria2c -o "$output" "$url"
+    else
+      youtube-dl --extract-audio --audio-format mp3 -o "$output" "$url"
+    fi
+  fi
+}
