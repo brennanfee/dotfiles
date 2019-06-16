d12f04e2b94bec47ffd6ce39b2c5acb917436e3a
Brennan Fee <brennan@github.mailbots.org>
2019-06-16 01:05:08 -0500

    Fixing path for VS Code

--- bash/windows-wsl/other/path.bash
+++ bash/windows-wsl/other/path.bash
@@ -2,4 +2,4 @@
 
 path_append "${SystemRoot}/System32"
 path_append "${SystemRoot}/System32/WindowsPowerShell/v1.0"
-path_append "${USERPROFILE}/AppData/Local/Programs/Microsoft VS Code"
+path_append "${USERPROFILE}/AppData/Local/Programs/Microsoft VS Code/bin"
