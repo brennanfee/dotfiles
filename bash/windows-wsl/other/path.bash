35a6b92a81d023cbd07afa14f05d7f8e7028c962
Brennan Fee <brennan@github.mailbots.org>
2019-06-16 00:50:07 -0500

    Adding some paths for WSL

--- bash/windows-wsl/other/path.bash
+++ bash/windows-wsl/other/path.bash
@@ -1,3 +1,5 @@
 # Append paths for Windows
 
 path_append "${SystemRoot}/System32"
+path_append "${SystemRoot}/System32/WindowsPowerShell/v1.0"
+path_append "${USERPROFILE}/AppData/Local/Programs/Microsoft VS Code"
