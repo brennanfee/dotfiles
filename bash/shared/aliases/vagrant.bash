da1da250d8e9dc1099f9c231d8cafda46d72f627
Brennan Fee <brennan@github.mailbots.org>
2020-04-21 20:15:33 -0500

    feat: Adding more vagrant aliases

--- bash/shared/aliases/vagrant.bash
+++ bash/shared/aliases/vagrant.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 alias vg='vagrant'
+alias vgs='vagrant ssh'
 alias vgu='vagrant up'
 alias vgh='vagrant halt'
+alias vgd='vagrant destroy'
+
+# and one for packer
+alias pkb='packer build'
