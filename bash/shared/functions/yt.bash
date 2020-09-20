fb726fa9f5d386a19c1f79b37fec620bb443e889
Brennan Fee <brennan@github.mailbots.org>
2020-09-19 20:25:39 -0500

    feat: File ID in youtube-dl downloads, format size
    To gaurd against duplicate names and the like, added in ID for the
    output name.  Also changed to be more specific about size formats.

--- bash/shared/functions/yt.bash
+++ bash/shared/functions/yt.bash
@@ -4,33 +4,32 @@ function yt-helper() {
   local url
   if [[ $IS_WSL == "1" ]]; then
     url=$(powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Get-Clipboard)
-    echo "Downloading $url"
-    if command_exists aria2c.exe; then
-      youtube-dl.exe --external-downloader=aria2c.exe -i -o "$1" "$url" "${@:2}"
-    else
-      youtube-dl.exe -i -o "$1" "$url" "${@:2}"
-    fi
   else
     url=$(xsel -o --clipboard)
-    echo "Downloading $url"
-    if command_exists aria2c; then
-      youtube-dl --external-downloader=aria2c -i -o "$1" "$url" "${@:2}"
-    else
-      youtube-dl -i -o "$1" "$url" "${@:2}"
-    fi
   fi
+
+  local highFormat="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]/bestvideo[ext=webm][height<=?1080]+bestaudio[ext=webm]/best[height<=?1080]/best"
+  local lowFormat="bestvideo[ext=mp4][height<=?720]+bestaudio[ext=m4a]/bestvideo[ext=webm][height<=?720]+bestaudio[ext=webm]/best[height<=?720]/best"
+  local format="$highFormat"
+
+  if [[ "$1" == "low" ]]; then
+    format="$lowFormat"
+  fi
+
+  echo "Downloading $url"
+  youtube-dl -i -f "$format" -o "$2" "$url" "${@:3}"
 }
 
 function yt() {
   local output
-  output="$(xdg-user-dir VIDEOS)/%(title)s.%(ext)s"
-  yt-helper "$output" "$@"
+  output="$(xdg-user-dir VIDEOS)/%(title)s-%(id)s.%(ext)s"
+  yt-helper "high" "$output" "$@"
 }
 
 function pt() {
   local output
-  output="$(xdg-user-dir PICTURES)/!other/%(title)s.%(ext)s"
-  yt-helper "$output" "$@"
+  output="$(xdg-user-dir PICTURES)/!other/%(title)s-%(id)s.%(ext)s"
+  yt-helper "low" "$output" "$@"
 }
 
 # To extract an MP3 from a video, requires ffmpeg to be installed
@@ -41,19 +40,10 @@ function ytm() {
   local url
   if [[ $IS_WSL == "1" ]]; then
     url=$(powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Get-Clipboard)
-    echo "Downloading $url"
-    if command_exists aria2c.exe; then
-      youtube-dl.exe --extract-audio --audio-format mp3 --external-downloader=aria2c.exe -i -o "$output" "$url" "${@:2}"
-    else
-      youtube-dl.exe --extract-audio --audio-format mp3 -i -o "$output" "$url" "${@:2}"
-    fi
   else
     url=$(xsel -o --clipboard)
-    echo "Downloading $url"
-    if command_exists aria2c; then
-      youtube-dl --extract-audio --audio-format mp3 --external-downloader=aria2c -i -o "$output" "$url" "${@:2}"
-    else
-      youtube-dl --extract-audio --audio-format mp3 -i -o "$output" "$url" "${@:2}"
-    fi
   fi
+
+  echo "Downloading $url"
+  youtube-dl --extract-audio --audio-format mp3 -i -o "$output" "$url" "${@:2}"
 }
