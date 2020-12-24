d7cb67b4ad9efd613db9282257cd9760f9356e32
Brennan Fee <brennan@github.mailbots.org>
2020-12-23 20:40:50 -0600

    Adding path loads for asdf Java and DotNet

--- bash/shared/third-party/asdf.bash
+++ bash/shared/third-party/asdf.bash
@@ -7,3 +7,14 @@ if [[ -d "$HOME/.asdf" ]]; then
   # shellcheck source=/dev/null
   source "$HOME/.asdf/completions/asdf.bash"
 fi
+
+# Java
+if [[ -f "$HOME/.asdf/plugins/java/set-java-home.bash" ]]; then
+  source "$HOME/.asdf/plugins/java/set-java-home.bash"
+fi
+
+# DotNet
+if [[ -f "$HOME/.asdf/plugins/dotnet/set-dotnet-home.bash" ]]; then
+  source "$HOME/.asdf/plugins/dotnet/set-dotnet-home.bash"
+fi
+
