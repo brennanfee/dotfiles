21db84a9e66cd1f983011af4e9b69191ed130640
Brennan Fee <brennan+gitlab@mailbots.org>
2022-05-28 22:21:12 -0500

    fix: Adding other XDG paths

--- bash/shared/exports/exports.bash
+++ bash/shared/exports/exports.bash
@@ -79,3 +79,6 @@ export FIGNORE=".git:.DS_Store"
 
 # XDG variablers, if not already set
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
