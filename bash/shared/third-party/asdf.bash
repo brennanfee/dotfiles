a2456ab6a9225be5b11c920803af7fc7f5bddbeb
Brennan Fee <brennan+gitlab@mailbots.org>
2022-06-15 13:28:41 -0500

    feat: Combining asdf settings
    Decided that all third-party settings should be grouped together rather
    than splitting out just the exports, for example.

--- bash/shared/third-party/asdf.bash
+++ bash/shared/third-party/asdf.bash
@@ -17,6 +17,33 @@ if ! ${SOURCED}; then
 fi
 # END Bash scrict mode
 
+#### Export environment variables for locations
+
+# Where all the files will live
+ASDF_CONFIG_HOME="$(xdg-base-dir CONFIG)/asdf"
+
+# The main config file
+ASDF_CONFIG_FILE="${ASDF_CONFIG_HOME}/asdfrc"
+
+# The location where asdf should be installed to and all the data lives
+ASDF_DATA_DIR="$(xdg-base-dir DATA)/asdf"
+
+# The default packages files
+ASDF_PYTHON_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-python-packages"
+ASDF_NPM_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-npm-packages"
+ASDF_GEM_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-gems"
+ASDF_GOLANG_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-golang-packages"
+
+export ASDF_CONFIG_HOME
+export ASDF_CONFIG_FILE
+export ASDF_DATA_DIR
+export ASDF_PYTHON_DEFAULT_PACKAGES_FILE
+export ASDF_NPM_DEFAULT_PACKAGES_FILE
+export ASDF_GEM_DEFAULT_PACKAGES_FILE
+export ASDF_GOLANG_DEFAULT_PACKAGES_FILE
+
+#### Completions and plugin scripts
+
 asdf_path="${ASDF_DATA_DIR:-${HOME}/.asdf}"
 
 if [[ -d ${asdf_path} ]]; then
