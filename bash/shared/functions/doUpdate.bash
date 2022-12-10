99ca6dadb58c68faf40dd7ad13a6716bc8387bec
Brennan Fee <brennan+gitlab@mailbots.org>
2022-12-10 12:49:47 -0600

    feat: Adding in asdf plugin updates do updater

--- bash/shared/functions/doUpdate.bash
+++ bash/shared/functions/doUpdate.bash
@@ -17,30 +17,51 @@ if ! ${SOURCED}; then
 fi
 # END Bash scrict mode
 
+function _write_msg() {
+  local bold
+  local reset
+  local green
+  local t_cols
+  t_cols=$(tput cols)
+  t_cols=$((t_cols - 1))
+  reset="$(tput sgr0)"
+  bold="$(tput bold)"
+  green="$(tput setaf 2)"
+  echo -e "${bold}${green}$1${reset}" | fold -sw "${t_cols}"
+}
+
 function doUpdate() {
-  if [[ "${OS_SECONDARY}" == "ubuntu" || "${OS_SECONDARY}" == "debian" ]]
+  if command_exists apt-get
   then
+    _write_msg "Getting updates with apt"
     DEBIAN_FRONTEND=noninteractive sudo apt-get -y -q update
     DEBIAN_FRONTEND=noninteractive sudo apt-get -y -q full-upgrade
     DEBIAN_FRONTEND=noninteractive sudo apt-get -y -q autoremove
-  else
-    echo "Unable to determine os or distribution."
   fi
 
   if command_exists flatpak
   then
+    _write_msg "Getting updates with flatpak"
     flatpak upgrade -y --noninteractive --system
     flatpak upgrade -y --noninteractive --user
   fi
 
   if command_exists snap
   then
+    _write_msg "Getting updates with snap"
     sudo snap refresh
   fi
 
   if command_exists pipx
   then
+    _write_msg "Getting updates with pipx"
     pipx upgrade-all
+  fi
+
+  if command_exists asdf
+  then
+    _write_msg "Updating plugins for asdf"
+    asdf plugin update --all
   fi
 }
 
