b5d67793a6b61a872d1fadb7039c065663d3fc12
Brennan Fee <brennan+gitlab@mailbots.org>
2023-05-01 14:49:08 -0500

    fix(wezterm): Setup bell settings, finalized others
    This should be the settings I want for Linux at least.  Once I try these
    settings on Windows I would expect there will be some more settings
    changes or a customer "Windows only" section.

--- rcs/config/wezterm/wezterm.lua
+++ rcs/config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 
 local config = {}
 
--- Appearance
+-- Fonts
 config.font = wezterm.font_with_fallback({
   { family="JetBrains Mono", weight="Regular", assume_emoji_presentation = false },
   { family="Symbols Nerd Font" },
@@ -11,14 +11,13 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 16.0
 config.adjust_window_size_when_changing_font_size = false
+config.freetype_interpreter_version = 40
 config.freetype_load_target = 'Light'
-
-config.color_scheme = "OneHalfDark"
-config.window_background_opacity = 1.0
-config.window_decorations = "RESIZE"
 
 config.bold_brightens_ansi_colors = "No"
 config.use_cap_height_to_scale_fallback_fonts = true
+config.strikethrough_position = "125%"
+config.underline_thickness = "2px"
 
 config.font_rules = {
   {
@@ -59,6 +58,30 @@ config.font_rules = {
   },
 }
 
+-- Color Scheme And Appearance
+config.color_scheme = "OneHalfDark"
+config.window_background_opacity = 1.0
+config.window_decorations = "RESIZE"
+config.force_reverse_video_cursor = true
+config.window_padding = {
+  left = 6,
+  right = 6,
+  top = 0,
+  bottom = 0,
+}
+
+-- Bell
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
 -- Initial state
 config.initial_rows = 30
 config.initial_cols = 130
@@ -66,6 +89,7 @@ config.default_cwd = wezterm.home_dir .. "/profile"
 config.default_prog = { "tmux", "new-session", "-c", wezterm.home_dir .. "/profile" }
 
 -- Turn off the tab bar, as I use tmux
+config.enable_tab_bar = false
 config.use_fancy_tab_bar = false
 config.show_tabs_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
@@ -73,6 +97,10 @@ config.show_new_tab_button_in_tab_bar = false
 -- Updates
 config.check_for_updates = true
 config.check_for_updates_interval_seconds = 86400 -- Every 24 hours
+
+-- Mouse Settings
+config.swallow_mouse_click_on_pane_focus = true
+config.swallow_mouse_click_on_window_focus = true
 
 -- Don't prompt when closing the window
 config.window_close_confirmation = "NeverPrompt"
