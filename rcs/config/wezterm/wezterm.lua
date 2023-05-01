1719cd15e815088bc6edcdbc51cf6e5031a8e44e
Brennan Fee <brennan+gitlab@mailbots.org>
2023-05-01 01:28:00 -0500

    feat(wezterm): Tweaked fonts for bold and italic

--- rcs/config/wezterm/wezterm.lua
+++ rcs/config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = {}
 
 -- Appearance
 config.font = wezterm.font_with_fallback({
-  "JetBrains Mono",
+  { family="JetBrains Mono", weight="Regular" },
   { family="Symbols Nerd Font", scale = 0.75 },
   "Noto Color Emoji",
 })
@@ -16,6 +16,60 @@ config.freetype_load_target = 'Light'
 config.color_scheme = "OneHalfDark"
 config.window_background_opacity = 1.0
 config.window_decorations = "RESIZE"
+
+config.bold_brightens_ansi_colors = "No"
+
+config.colors = {
+  brights = {
+    '#282c34',
+    '#e06c75',
+    '#98c379',
+    '#ff0000',
+    '#61afef',
+    '#c678dd',
+    '#56b6c2',
+    '#dcdfe4',
+  },
+}
+
+config.font_rules = {
+  {
+    intensity = 'Bold',
+    italic = false,
+    font = wezterm.font_with_fallback({
+      { family="JetBrains Mono", weight="ExtraBold", italic=false },
+      { family="Symbols Nerd Font", scale = 0.75 },
+      "Noto Color Emoji",
+    }),
+  },
+  {
+    intensity = 'Bold',
+    italic = true,
+    font = wezterm.font_with_fallback({
+      { family="JetBrains Mono", weight="ExtraBold", italic=true },
+      { family="Symbols Nerd Font", scale = 0.75 },
+      "Noto Color Emoji",
+    }),
+  },
+  {
+    intensity = 'Half',
+    italic = false,
+    font = wezterm.font_with_fallback({
+      { family="JetBrains Mono", weight="Thin", italic=false },
+      { family="Symbols Nerd Font", scale = 0.75 },
+      "Noto Color Emoji",
+    }),
+  },
+  {
+    intensity = 'Half',
+    italic = true,
+    font = wezterm.font_with_fallback({
+      { family="JetBrains Mono", weight="Thin", italic=true },
+      { family="Symbols Nerd Font", scale = 0.75 },
+      "Noto Color Emoji",
+    }),
+  },
+}
 
 -- Initial state
 config.initial_rows = 30
