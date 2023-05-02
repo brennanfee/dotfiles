632dbab8b530da86350b939cc36edd46770ac087
Brennan Fee <brennan+gitlab@mailbots.org>
2023-05-02 09:22:43 -0500

    chore(wezterm): Reformatted the lua

--- rcs/config/wezterm/wezterm.lua
+++ rcs/config/wezterm/wezterm.lua
@@ -5,14 +5,18 @@ local config = {}
 
 -- Fonts
 config.font = wezterm.font_with_fallback({
-  { family="JetBrains Mono", weight="Regular", assume_emoji_presentation = false },
-  { family="Symbols Nerd Font" },
-  { family="Noto Color Emoji" },
+  {
+    family = "JetBrains Mono",
+    weight = "Regular",
+    assume_emoji_presentation = false,
+  },
+  { family = "Symbols Nerd Font" },
+  { family = "Noto Color Emoji" },
 })
 config.font_size = 16.0
 config.adjust_window_size_when_changing_font_size = false
 config.freetype_interpreter_version = 40
-config.freetype_load_target = 'Light'
+config.freetype_load_target = "Light"
 
 config.bold_brightens_ansi_colors = "No"
 config.use_cap_height_to_scale_fallback_fonts = true
@@ -21,39 +25,39 @@ config.underline_thickness = "2px"
 
 config.font_rules = {
   {
-    intensity = 'Bold',
+    intensity = "Bold",
     italic = false,
     font = wezterm.font_with_fallback({
-      { family="JetBrains Mono", weight="ExtraBold", italic=false },
-      { family="Symbols Nerd Font" },
-      { family="Noto Color Emoji" },
+      { family = "JetBrains Mono", weight = "ExtraBold", italic = false },
+      { family = "Symbols Nerd Font" },
+      { family = "Noto Color Emoji" },
     }),
   },
   {
-    intensity = 'Bold',
+    intensity = "Bold",
     italic = true,
     font = wezterm.font_with_fallback({
-      { family="JetBrains Mono", weight="ExtraBold", italic=true },
-      { family="Symbols Nerd Font" },
-      { family="Noto Color Emoji" },
+      { family = "JetBrains Mono", weight = "ExtraBold", italic = true },
+      { family = "Symbols Nerd Font" },
+      { family = "Noto Color Emoji" },
     }),
   },
   {
-    intensity = 'Half',
+    intensity = "Half",
     italic = false,
     font = wezterm.font_with_fallback({
-      { family="JetBrains Mono", weight="Thin", italic=false },
-      { family="Symbols Nerd Font" },
-      { family="Noto Color Emoji" },
+      { family = "JetBrains Mono", weight = "Thin", italic = false },
+      { family = "Symbols Nerd Font" },
+      { family = "Noto Color Emoji" },
     }),
   },
   {
-    intensity = 'Half',
+    intensity = "Half",
     italic = true,
     font = wezterm.font_with_fallback({
-      { family="JetBrains Mono", weight="Thin", italic=true },
-      { family="Symbols Nerd Font" },
-      { family="Noto Color Emoji" },
+      { family = "JetBrains Mono", weight = "Thin", italic = true },
+      { family = "Symbols Nerd Font" },
+      { family = "Noto Color Emoji" },
     }),
   },
 }
@@ -63,30 +67,28 @@ config.color_scheme = "OneHalfDark"
 config.window_background_opacity = 1.0
 config.window_decorations = "RESIZE"
 config.force_reverse_video_cursor = true
-config.window_padding = {
-  left = 6,
-  right = 6,
-  top = 0,
-  bottom = 0,
-}
+config.window_padding = { left = 10, right = 10, top = 10, bottom = 5 }
 
 -- Bell
 config.audible_bell = "Disabled"
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
+  fade_in_function = "EaseIn",
   fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
+  fade_out_function = "EaseOut",
   fade_out_duration_ms = 150,
 }
-config.colors = {
-  visual_bell = '#202020',
-}
+config.colors = { visual_bell = "#202020" }
 
 -- Initial state
 config.initial_rows = 30
 config.initial_cols = 130
 config.default_cwd = wezterm.home_dir .. "/profile"
-config.default_prog = { "tmux", "new-session", "-c", wezterm.home_dir .. "/profile" }
+config.default_prog = {
+  "tmux",
+  "new-session",
+  "-c",
+  wezterm.home_dir .. "/profile",
+}
 
 -- Turn off the tab bar, as I use tmux
 config.enable_tab_bar = false
