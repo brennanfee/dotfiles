79362f40ad0de36244ac63de1891298855835e8a
Brennan Fee <brennan.git@mailbots.org>
2023-07-11 20:38:28 -0500

    feat: Aligned vagrant aliases, added packer cmds

--- bash/shared/aliases/vagrant.bash
+++ bash/shared/aliases/vagrant.bash
@@ -17,11 +17,79 @@ if ! ${SOURCED}; then
 fi
 # END Bash scrict mode
 
-alias vg='vagrant'
-alias vgs='vagrant ssh'
-alias vgu='vagrant up'
-alias vgh='vagrant halt'
-alias vgd='vagrant destroy'
+function vagrant_cmd() {
+  # Check we are in a directory with a Vagrantfile
+  if [[ ! -f "./Vagrantfile" ]]; then
+    error_msg "Not in a directory with a Vagrantfile!"
+  fi
 
-# and one for packer
-alias pkb='packer build'
+  vagrant "$@"
+}
+
+function vagrant_provision() {
+  local provisioner="main"
+  if [[ $# -eq 0 ]]; then
+    print_warning "No provisioner given, using default of 'main'"
+  else
+    provisioner="$1"
+  fi
+
+  vagrant_cmd provision --provision-with "${provisioner}"
+}
+
+alias v='vagrant_cmd'
+alias vp='vagrant_provision'
+alias vc='vagrant_cmd ssh'
+alias vssh='vagrant_cmd ssh'
+alias vu='vagrant_cmd up'
+alias vh='vagrant_cmd halt'
+alias vd='vagrant_cmd destroy'
+alias vss='vagrant_cmd snapshot save'
+alias vsr='vagrant_cmd snapshot restore'
+alias vsd='vagrant_cmd snapshot destroy'
+
+### For Packer
+
+function packer_build() {
+  # Check we are in a directory packer directory
+  if [[ ! -f "./build.bash" ]]; then
+    error_msg "Not in a packer build directory!"
+  fi
+
+  ./build.bash "$@"
+}
+
+function packer_clean() {
+  # Check we are in a directory packer directory
+  if [[ ! -f "./clean.bash" ]]; then
+    error_msg "Not in a packer build or test directory!"
+  fi
+
+  ./clean.bash "$@"
+}
+
+function packer_test() {
+  # Check we are in a directory packer directory
+  if [[ ! -f "./test.bash" ]]; then
+    error_msg "Not in a packer test directory!"
+  fi
+
+  "./test.bash" "$@"
+}
+
+function packer_run_all_tests() {
+  # Check we are in a directory packer directory
+  if [[ ! -f "./run-all-tests.bash" ]]; then
+    error_msg "Not in a packer test directory!"
+  fi
+
+  "./run-all-tests.bash" "$@"
+}
+
+alias pb='packer_build'
+alias pbh='packer_build -h'
+alias pc='packer_clean'
+alias pch='packer_clean -h'
+alias pt='packer_test'
+alias pth='packer_test -h'
+alias pall='packer_run_all_tests'
