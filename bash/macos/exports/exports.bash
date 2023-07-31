448b7ed6f7463767d4d1de6a6d8feed6426b2912
Brennan Fee <brennan.git@mailbots.org>
2023-07-31 17:40:24 -0500

    fix: Removed Github API token for Homebrew
    The token was deleted a long time ago but I left the exported shell
    variable in my config.  Furthermore, if I still needed this it should
    not be here in the public dotfiles but placed into my private dotfiles
    repo instead.

--- bash/macos/exports/exports.bash
+++ bash/macos/exports/exports.bash
@@ -18,5 +18,3 @@ fi
 # END Bash scrict mode
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
-
-export HOMEBREW_GITHUB_API_TOKEN=d4937742c2249cdeddd3969c46d82069e6a666b9
