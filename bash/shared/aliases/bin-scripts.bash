b27e408b569aa83bd949973e933030e284cf9db0
Brennan Fee <brennan.git@mailbots.org>
2024-03-04 11:56:05 -0600

    feat: Making doUpdate a script not a shell function

---
+++ bash/shared/aliases/bin-scripts.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Bash strict mode
+# shellcheck disable=SC2154
+([[ -n ${ZSH_EVAL_CONTEXT} && ${ZSH_EVAL_CONTEXT} =~ :file$ ]] \
+  || [[ -n ${BASH_VERSION} ]] && (return 0 2> /dev/null)) && SOURCED=true || SOURCED=false
+if ! ${SOURCED}; then
+  set -o errexit  # same as set -e
+  set -o nounset  # same as set -u
+  set -o errtrace # same as set -E
+  set -o pipefail
+  set -o posix
+  #set -o xtrace # same as set -x, turn on for debugging
+
+  shopt -s extdebug
+  IFS=$(printf '\n\t')
+fi
+# END Bash scrict mode
+
+if [[ -x "${DOTFILES}/bin/do-update" ]]; then
+  alias doup='${DOTFILES}/bin/do-update'
+fi
