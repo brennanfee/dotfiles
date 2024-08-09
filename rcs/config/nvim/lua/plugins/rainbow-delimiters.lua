9449fb930ed2d47d959e9e1296d41482c21fb742
Brennan Fee <brennan.git@mailbots.org>
2024-08-09 09:16:50 -0500

    fix(nvim): Optimized perf for rainbow delimiters

--- rcs/config/nvim/lua/plugins/rainbow-delimiters.lua
+++ rcs/config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -8,10 +8,15 @@ function M.config()
   require("rainbow-delimiters.setup").setup({
     strategy = {
       [""] = rainbow_delimiters.strategy["global"],
+      vim = rainbow_delimiters.strategy["local"],
     },
     query = {
       [""] = "rainbow-delimiters",
-      -- lua = "rainbow-blocks",
+      lua = "rainbow-blocks",
+    },
+    priority = {
+      [""] = 110,
+      lua = 210,
     },
     highlight = {
       "RainbowDelimiterRed",
