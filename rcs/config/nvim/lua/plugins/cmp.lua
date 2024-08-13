1e1ba9d31b2d98ae475f3b2e5af4b246159e3fcc
Brennan Fee <brennan.git@mailbots.org>
2024-08-13 09:08:56 -0500

    fix(nvim): Disable tab from activating intellisense

--- rcs/config/nvim/lua/plugins/cmp.lua
+++ rcs/config/nvim/lua/plugins/cmp.lua
@@ -114,36 +114,36 @@ function M.config()
       -- Accept currently selected item. If none selected, `select` first item.
       -- Set `select` to `false` to only confirm explicitly selected items.
       ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif check_backspace() then
-          fallback()
-          -- require("neotab").tabout()
-        else
-          fallback()
-          -- require("neotab").tabout()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
+      -- ["<Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_next_item()
+      --   elseif luasnip.expandable() then
+      --     luasnip.expand()
+      --   elseif luasnip.expand_or_jumpable() then
+      --     luasnip.expand_or_jump()
+      --   elseif check_backspace() then
+      --     fallback()
+      --     -- require("neotab").tabout()
+      --   else
+      --     fallback()
+      --     -- require("neotab").tabout()
+      --   end
+      -- end, {
+      --   "i",
+      --   "s",
+      -- }),
+      -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_prev_item()
+      --   elseif luasnip.jumpable(-1) then
+      --     luasnip.jump(-1)
+      --   else
+      --     fallback()
+      --   end
+      -- end, {
+      --   "i",
+      --   "s",
+      -- }),
     }),
     formatting = {
       fields = { "kind", "abbr", "menu" },
