69f957bcab5e1cf48b2dc878b62c963cac35255a
Brennan Fee <brennan.git@mailbots.org>
2024-10-14 13:45:08 -0500

    fix: Renamed commitlint config file (to be hidden)
    Newer version of commitlint supports hidden (dotfile) configuration file.

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -1,7 +1,10 @@
-// commitlint.config.js
-module.exports = {
+// CommitLint Configuration - .commitlintrc.js
+const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [2, "always", ["lower-case", "sentence-case", "start-case"]],
   },
+  defaultIgnores: true,
 };
+
+export default Configuration;
