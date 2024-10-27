f22aecb68e60edee3ebb8843949fcd7583c262b0
Brennan Fee <brennan.git@mailbots.org>
2024-10-27 00:35:53 -0500

    fix(asdf): Moved all bash setup into "first" script
    ASDF wasn't reading some of the settings because I was setting the environment variables after
    calling the asdf.sh init script. Also updated tool-versions. Refactored the default package lists
    along with adding one for Rust.

--- bash/shared/first/01-asdf.bash
+++ bash/shared/first/01-asdf.bash
@@ -31,6 +31,36 @@ fi
 
 export ASDF_DATA_DIR
 
+# Where all the files will live
+ASDF_CONFIG_HOME="$(xdg-base-dir CONFIG)/asdf"
+export ASDF_CONFIG_HOME
+
+# The main config file
+export ASDF_CONFIG_FILE="${ASDF_CONFIG_HOME}/asdfrc"
+
+# Plugin specific settings
+
+## Python
+# ASDF_PYTHON_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-python-packages"
+
+## Dotnet
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+## Node
+export ASDF_NPM_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-node-packages"
+export ASDF_NODEJS_AUTO_ENABLE_COREPACK=""
+export ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY="latest_available"
+
+## Ruby
+export ASDF_GEM_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-ruby-packages"
+
+## Golang
+export ASDF_GOLANG_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-golang-packages"
+export ASDF_GOLANG_MOD_VERSION_ENABLED="true"
+
+## Rust
+export ASDF_CRATE_DEFAULT_PACKAGES_FILE="${ASDF_CONFIG_HOME}/default-rust-packages"
+
 #### Completions and plugin scripts
 
 if [[ -d "${ASDF_DATA_DIR}" ]]; then
@@ -39,4 +69,56 @@ if [[ -d "${ASDF_DATA_DIR}" ]]; then
 
   # shellcheck source=/dev/null
   source_if "${ASDF_DATA_DIR}/completions/asdf.bash"
+
+  # Java
+  source_if "${ASDF_DATA_DIR}/plugins/java/set-java-home.bash"
+
+  # DotNet
+  source_if "${ASDF_DATA_DIR}/plugins/dotnet/set-dotnet-env.bash"
+
+  # Go
+  source_if "${ASDF_DATA_DIR}/plugins/golang/set-env.bash"
+fi
+
+# fzf integration
+if command_exists fzf; then
+  vmi() {
+    local lang=${1}
+
+    if [[ -z ${lang} ]]; then
+      lang=$(asdf plugin-list | fzf)
+    fi
+
+    if [[ -n ${lang} ]]; then
+      local versions
+      versions=$(asdf list-all "${lang}" | fzf --tac --no-sort --multi)
+      if [[ -n ${versions} ]]; then
+        # shellcheck disable=2116
+        for version in $(echo "${versions}"); do
+          asdf install "${lang}" "${version}"
+        done
+      fi
+    fi
+  }
+
+  vmu() {
+    local lang=${1}
+
+    if [[ -z ${lang} ]]; then
+      lang=$(asdf plugin-list | fzf)
+    fi
+
+    if [[ -n ${lang} ]]; then
+      local versions
+      versions=$(asdf list "${lang}" | fzf -m)
+      if [[ -n ${versions} ]]; then
+        # shellcheck disable=2116
+        for version in $(echo "${versions}"); do
+          asdf uninstall "${lang}" "${version}"
+        done
+      fi
+    fi
+  }
+
+  # TODO: Consider adding others
 fi
