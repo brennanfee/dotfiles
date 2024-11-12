0497050d29bf1c55c1b6a24e27f6694ffab1b220
Brennan Fee <brennan.git@mailbots.org>
2024-11-11 21:13:36 -0600

    feat(bash): Add support for nix package mgr and direnv
    Early support for nix package manager, still more to do there before it can fully be used. Also
    added in support for direnv, if installed. Reordered the sequence of bash directories for
    configuring the environment, put aliases sooner and functions later.

---
+++ bash/shared/first/03-nix.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Bash strict mode
+# shellcheck disable=SC2154
+([[ -n ${ZSH_EVAL_CONTEXT} && ${ZSH_EVAL_CONTEXT} =~ :file$ ]] \
+  || [[ -n ${BASH_VERSION} ]] && (return 0 2> /dev/null)) && SOURCED=true || SOURCED=false
+if ! ${SOURCED}; then
+  set -o errexit  # same as set -e
+  set -o nounset  # same as set -u
+  set -o errtrace # same as set -E
+  set -o pipefail
+  set -o posix
+  #set -o xtrace # same as set -x, turn on for debugging
+
+  shopt -s extdebug
+  IFS=$(printf '\n\t')
+fi
+# END Bash strict mode
+
+if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]]; then
+  source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+  export NIX_REMOTE=daemon
+fi
