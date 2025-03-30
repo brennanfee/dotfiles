f3cd4dc53944487af5f9b2974fe6b353f1912697
Brennan Fee <brennan.git@mailbots.org>
2025-03-29 22:06:59 -0500

    feat(commitlint): Updating CommitLint settings
    Updating CommitLint config to more closely match editorconfig's settings.  Chiefly allowing body max line length to be
    up to 120 characters.

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -2,6 +2,7 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "body-max-line-length": [2, "always", 120],
     "subject-case": [2, "always", ["lower-case", "sentence-case", "start-case"]],
   },
   defaultIgnores: true,
