5f259b260fca816ce04ab9cab84746ccc3d0acfa
Brennan Fee <brennan.git@mailbots.org>
2025-04-08 09:37:10 -0500

    feat(history): Attempting to merge history between sessions
    Attempting to have a common history across terminal sessions that gets updated 'live', as opposed to only when a session
    closes.  Also attempting to prevent duplicate entries from being included.

--- bash/shared/exports/history.bash
+++ bash/shared/exports/history.bash
@@ -17,11 +17,43 @@ if ! ${SOURCED}; then
 fi
 # END Bash strict mode
 
-export HISTCONTROL=${HISTCONTROL:-}${HISTCONTROL+,}ignoredups
-export HISTCONTROL=${HISTCONTROL:-}${HISTCONTROL+,}ignorespace
-export HISTIGNORE=$'[ \t]*:&:[fb]g:exit:ls:ls -la:sl:ll:la:lls:lla:pwd:cd:cdp:cdpp:cdd:cdi:cdt:cdtp:cdm:cdmp:cdv:cdb:cdc:cdx:cdh:cdr:cdw:* --help *'
-export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S  "
-export HISTFILESIZE=5000
-export HISTSIZE=5000
+# For other history ideas check out these articles:
+#
+# https://unix.stackexchange.com/questions/18212/bash-history-ignoredups-and-erasedups-setting-conflict-with-common-history
+#
+# https://askubuntu.com/questions/80371/bash-history-handling-with-multiple-terminals
+#
+# https://unix.stackexchange.com/questions/48713/how-can-i-remove-duplicates-in-my-bash-history-preserving-order
+#
+
+HISTCONTROL="ignoreboth:erasedups"
+HISTIGNORE="[ \t]*:[fb]g:exit:ls:ls -la:sl:ll:la:lls:lla:pwd:cd:cd -:cdp:cdpp:cdd:cdi:cdt:cdtp:cdm:cdmp"
+HISTIGNORE+=":cdv:cdb:cdc:cdx:cdh:cdr:cdw:* --help:* -h:history:history *:man:man *:date"
+# HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S  "
+HISTFILESIZE=20000
+HISTSIZE=10000
 HISTFILE="$(xdg_base_dir STATE)/bash_history"
-export HISTFILE
+
+function historyclean {
+  if [[ -e "${HISTFILE}" ]]; then
+    local history_lock
+    exec {history_lock}< "${HISTFILE}" && flock -s ${history_lock}
+    tac "${HISTFILE}" | awk '!x[$0]++' | tac > "${HISTFILE}.tmp$$"
+    history -a
+    mv -f "${HISTFILE}.tmp$$" "${HISTFILE}"
+    history -c
+    history -r
+    flock -u ${history_lock}
+  fi
+}
+
+function historymerge {
+  history -n
+  history -w
+  history -c
+  history -r
+}
+
+trap historymerge EXIT
+
+PROMPT_COMMAND="historyclean;${PROMPT_COMMAND}"
