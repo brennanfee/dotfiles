0092ebbbda692d15fb4721f11e62c13f3dc66ecb
Brennan Fee <brennan.git@mailbots.org>
2025-04-10 12:28:15 -0500

    chore(nvim): Cleaning up by removing nvim-test config
    I still support multiple possible configs for neovim. However, I really only want my current one in my dotfiles.

--- rcs/config/nvim-test/init.lua
+++
@@ -1,9 +0,0 @@
-require("core.asdf-support")
-require("core.options")
-require("core.options-gui")
-require("core.keymaps")
-require("core.autocmds")
-
-require("plugin-loader")
-
-require("core.lazy")
