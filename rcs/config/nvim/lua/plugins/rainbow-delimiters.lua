f51c2a34fcbe4bcfb174e7d5c5264275ad4181f3
Brennan Fee <brennan.git@mailbots.org>
2025-04-26 00:14:45 -0500

    fix(theme): Fixed rainbow delimiter colors for Catppuccin
    Needed to manually copy over the colors for rainbow delimiters.

--- rcs/config/nvim/lua/plugins/rainbow-delimiters.lua
+++ rcs/config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -30,13 +30,13 @@ function M.config()
   })
 
   -- TODO: Use theme colors
-  vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#56B6C2" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#f38ba8" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#fab387" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#89b4fa" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#f2cdcd" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#a6e3a1" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#cba6f7" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#74c7ec" })
 end
 
 return M
