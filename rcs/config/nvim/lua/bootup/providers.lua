26fe406eefba6ea59c8f9ea65c85cc0daea2f42d
Brennan Fee <brennan.git@mailbots.org>
2025-05-29 17:09:22 -0500

    fix(nvim): Fix proper provider loading

--- rcs/config/nvim/lua/bootup/providers.lua
+++ rcs/config/nvim/lua/bootup/providers.lua
@@ -1,4 +1,18 @@
+local utils = require("tools.utils")
+
 vim.loader.enable()
 
 -- Turn off the Perl provider, we'll never use it
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+
+local python_path = utils.getSystemExe("python3")
+if utils.isNotEmpty(python_path) then
+  vim.g.python3_host_prog = python_path
+  vim.g.loaded_python3_provider = 1
+else
+  -- Fall back to system python, which should always be there
+  vim.g.python3_host_prog = "/usr/bin/python3"
+  vim.g.loaded_python3_provider = 1
+end
--- rcs/config/nvim/lua/tools/utils.lua
+++ rcs/config/nvim/lua/tools/utils.lua
@@ -10,12 +10,33 @@ M.isEmpty = function(s)
   return s == nil or s == ""
 end
 
+M.trimString = function(s)
+  return s:match("^()%s*$") and "" or s:match("^%s*(.*%S)")
+end
+
+M.stringStartsWith = function(s, start)
+  return s:sub(1, #start) == start
+end
+
 -- Append a table to another table
 M.tableAppendList = function(t1, t2)
   for _, v in pairs(t2) do
     table.insert(t1, v)
   end
   return t1
+end
+
+M.systemNotification = function(s)
+  vim.fn.system("notify-send '" .. s .. "'")
+end
+
+M.getSystemExe = function(s)
+  local path = M.trimString(vim.fn.system("mise which " .. s))
+  if M.isEmpty(path) or M.stringStartsWith(path, "mise ERROR") then
+    path = M.trimString(vim.fn.system("which " .. s))
+  end
+
+  return path
 end
 
 return M
